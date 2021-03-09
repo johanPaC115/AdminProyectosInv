134708389ba241d548cde7dc058d880d695df44f
johanPaC115 <johanandrescp@ufps.edu.co>
2021-03-09 12:01:51 -0500

    Config Lang es, timezone america/Bogota, enebled debug mode

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    |Las siguientes líneas de idioma se utilizan durante la autenticación para varios
+    |mensajes que debemos mostrar al usuario. Eres libre de modificar
+    |estas líneas de idioma de acuerdo con los requisitos de su aplicación.
+    |
+    */
+
+    'failed'   => 'Estas credenciales no coinciden con nuestros registros.',
+    'password' => 'La contraseña ingresada no es correcta.',
+    'throttle' => 'Demasiados intentos de acceso. Por favor intente nuevamente en :seconds segundos.',
+];
---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Líneas de idioma de recordatorio de contraseña
+    |--------------------------------------------------------------------------
+    |
+    | Las siguientes líneas de idioma son las líneas predeterminadas que coinciden con las razones
+    | que proporciona el intermediario de contraseñas para un intento de actualización de contraseña
+    | ha fallado, como por un token no válido o una nueva contraseña no válida.
+    |
+    */
+
+    'reset'     => '¡Su contraseña ha sido restablecida!',
+    'sent'      => '¡Le hemos enviado por correo electrónico el enlace para restablecer su contraseña!',
+    'throttled' => 'Por favor espere antes de intentar de nuevo.',
+    'token'     => 'El token de restablecimiento de contraseña es inválido.',
+    'user'      => 'No encontramos ningún usuario con ese correo electrónico.',
+];
